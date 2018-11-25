fb527b679877907993a5d71707139087a45a876a
rbingen3 <32225722+rbingen3@users.noreply.github.com>
2018-11-25 16:13:06 -0600

    Add Stop.java after it was missed

---
+++ src/Stop.java
@@ -0,0 +1,27 @@
+public class Stop 
+{
+	public int id;
+	public String name;
+	public int numRiders;
+	public double latitude;
+	public double longitude;
+	
+	public Stop (int id, String name, int numRiders, double latitude, double longitude)
+	{
+		this.id = id;
+		this.name = name;
+		this.numRiders = numRiders;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	public void addPassengersToStop(int num)
+	{
+		numRiders += num;
+	}
+	
+	public void passengersGetOnBus(int num)
+	{
+		numRiders -= num;
+	}
+}
