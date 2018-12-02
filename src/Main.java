ee8dc27e40272aa1081bf69ee00b9bfdbf5cc888
Shakiem <shakiem_saunders@hotmail.com>
2018-12-02 11:54:42 -0500

    Adding better error handling to the reading of he configuration file if the user does not provide one in execution

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main
 
 	public static void main(String[] args) 
 	{
+		if(args.length < 1){
+			System.out.println("Configuration file not specified, exiting simulation.");
+			System.exit(0);
+		}
+
 		Simulation sim = new Simulation(args[0],20);
 	}
 
